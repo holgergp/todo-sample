0cbadce48db5f6d6249afe40fb24107f9e0ac71f
Holger Grosse-Plankermann <holgergp@gmail.com>
2022-07-15 09:00:27 +0200

    Check items

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,22 +1,31 @@
 import TodoItem from "../TodoItem/TodoItem";
 
 import styles from "./Todo.module.scss";
+import { useState } from "react";
 
 export interface TodoType {
+  id: string;
   name: string;
   checked: boolean;
 }
 
 const sampleTodos: TodoType[] = [
-  { name: "Hacken", checked: false },
-  { name: "Kochen", checked: false },
+  { id: "0", name: "Hacken", checked: false },
+  { id: "1", name: "Kochen", checked: false },
 ];
 
 const Todo = (): JSX.Element => {
+  const [items, setItems] = useState(sampleTodos);
+  const toggleCheckStatusOfAnItem = (id: string) => {
+    const toggledItems = items.map((item) => {
+      return item.id === id ? { ...item, checked: !item.checked } : item;
+    });
+    setItems(toggledItems);
+  };
   return (
     <div className={styles.todoGrid}>
-      {sampleTodos.map((todo, index) => (
-        <TodoItem key={index} todo={todo}></TodoItem>
+      {items.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} toggleCheckStatus={toggleCheckStatusOfAnItem}></TodoItem>
       ))}
     </div>
   );
